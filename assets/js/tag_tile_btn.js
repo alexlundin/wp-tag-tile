deba32a7d69f0b5e66c8bcdd22638938546db622
Alex <aslundin@yandex.by>
2020-12-09 12:40:49 +0300

    update scripts

--- assets/js/tag_tile_btn.js
+++ assets/js/tag_tile_btn.js
@@ -4,15 +4,15 @@
         postsValuesTiles.push({text:value, value:key});
     });
     tinymce.create("tinymce.plugins.true_mce_button", {
-        init : function(ed) {
-            ed.addButton("true_mce_button", {
+        init : function(editor) {
+            editor.addButton("true_mce_button", {
                 type: 'listbox',
                 text: 'Метки тегов',
                 values: postsValuesTiles,
                 onselect: function() {
                     let value = this.value();
                     let return_t = '[tiles id=' + value + ']';
-                    ed.execCommand("mceInsertContent", 0, return_t);
+                    editor.execCommand("mceInsertContent", 0, return_t);
                 }
             });
         },

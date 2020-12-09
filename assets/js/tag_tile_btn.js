63e5b55185b2cbc0372aafbb9502ef4ae150212c
Alex <aslundin@yandex.by>
2020-12-09 13:18:59 +0300

    update scripts

--- assets/js/tag_tile_btn.js
+++ assets/js/tag_tile_btn.js
@@ -1,14 +1,14 @@
 (function() {
     const postsValuesTiles = [];
     jQuery.each(postsValues_tiles_button, function(key, value) {
-        postsValuesTiles.push({text:value, val:key});
+        valuesTiles.push({text:value, val:key});
     });
     tinymce.create("tinymce.plugins.true_mce_button", {
         init : function(editor) {
             editor.addButton("true_mce_button", {
                 type: 'listbox',
                 text: 'Метки тегов',
-                values: postsValuesTiles,
+                values: valuesTiles,
                 onselect: function() {
                     let value = this.val();
                     let return_t = '[tiles id=' + value + ']';
